eff4419c1a0396925dd335569fd1a10f8897258c
trypravin07-web <trypravin07@gmail.com>
2026-01-22 21:50:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+
+from flask import Flask, render_template, request, send_file
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        file = request.files["file"]
+        df = pd.read_excel(file)
+
+        df_b = pd.DataFrame({
+            "store_id": df["store_identifier"],
+            "sku_id": df["lookup_code"],
+            "name": df["item_name"],
+            "size": df["size"],
+            "description": df["item_details"],
+            "cost_unit": df["cost_unit"],
+            "price": df["price"],
+            "department": df["department"],
+            "aisle": df["aisle"],
+            "image_URL": df["remote_image_URL"]
+        })
+
+        output = "B_converted.xlsx"
+        df_b.to_excel(output, index=False)
+        return send_file(output, as_attachment=True)
+
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run()
